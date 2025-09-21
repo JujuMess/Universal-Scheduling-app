5e25114334dd7d3b98b71ff02e5be7ba37af5b7f
Julien <messinajulienpro@gmail.com>
2025-09-21 16:04:28 +0100

    adding guidelines inside the app file

--- app/universal_scheduling_app.py
+++ app/universal_scheduling_app.py
@@ -14,3 +14,15 @@ def normalise(text):
         print()
         return text
     
+# create import_sheet() function
+    # should be openpyxl to load the workbook based on the sheet entered as the function argument
+#create load_employee_list() function
+    # create a pandas dataframe from the sheet with a column for each one in the sheet, all names normalised
+#create load_holiday_calendar() function
+    # create a pandas dataframe from the sheet with a column for each one in the sheet, all names normalised
+#create load_shift_requirements() function
+    # create a pandas dataframe from the sheet with a column for each one in the sheet, all names normalised
+#create load_history_file() function
+    # create a pandas dataframe from the sheet with a column for each one in the sheet, all names normalised
+#create check_name_list() function
+    #check all names in all dfs are mathing
